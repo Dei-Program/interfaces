8434d851e33fe4d7e35880de5665eacd6efd3bad
Anderson Revelo <fabiander_21@hotmail.es>
2018-01-20 13:27:06 -0500

    Comentario AR

--- interfaces/WindowsFormsApplication3/Program.cs
+++ interfaces/WindowsFormsApplication3/Program.cs
@@ -16,6 +16,7 @@ namespace WindowsFormsApplication3
         [STAThread]
         static void Main()
         {
+            //lalalalalalalalalalaalala jamejame ja
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MenuPrincipal());
